c3e81cc3106094a1c71faf3a1b17e56a9d6c7986
pinkkykuki <panpink1509@gmail.com>
2026-01-09 15:10:52 +0700

    Update lab13_2.cpp

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,40 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[][M], int s , int x, int y) {
+    
+    for (int i = 0; i < N; i++) {
+        if (s,x,y == 0) {
+            break;
+        } else {
+        for (int j = 0; j < M; j++) {
+            if (sqrt(pow(i-x,2)+pow(j-y,2)) <= s-1) {
+                image[i][j] = 1;
+            }
+        }
+        }
+    }                                                       
+}
+void showImage(const bool image[][M]) {
+    for (int i = 0; i < M+2; i++) {
+        cout << "-";
+    }
+    cout << endl;
+    for (int i = 0; i < N; i++) {
+        cout << "|";
+        for (int j = 0; j < M; j++) {
+            if (image[i][j] == 1) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << "|";
+        cout << endl;
+    }
+    for (int i = 0; i < M+2; i++) {
+        cout << "-";
+    }
+    cout << endl;
+}           
